aceaee4a65f21fb9c8bbedefca01429903bc4ff7
lzalit238 <lzalit238@west-mec.org>
2021-09-20 15:22:11 -0700

    Create and Update SCSS

---
+++ tabs/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
+
+function App() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
